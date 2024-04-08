29bc42e24a8ce45b64b45033954e9841165093a1
Amazingren <493392637@qq.com>
2024-04-08 16:45:36 +0200

    exps: part seg for sc_sp & sp

--- devs/baseline/segmentation/seg.sh
+++ devs/baseline/segmentation/seg.sh
@@ -1,5 +1,5 @@
 python main.py \
     --root /workspace/wwang/BINGO/datasets/point_cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
-    --gpu 6 \
-    --ckpts ../experiments/base/pretrain/Recon_SM_reproduce/ckpt-epoch-300.pth \
+    --gpu 5 \
+    --ckpts ../experiments/base/pretrain/dev_debug/ckpt-epoch-300.pth \
     --seed 0
--- devs/baseline/segmentation/test.sh
+++ devs/baseline/segmentation/test.sh
@@ -1,1 +1,4 @@
-python main.py --gpu $1 --log_dir $2 --ckpts $3 --seed $RANDOM --test
+python main.py --gpu 5 \
+     --log_dir $2 \
+     --ckpts $3 --seed $RANDOM --test
+# python main.py --gpu $1 --log_dir $2 --ckpts $3 --seed $RANDOM --test
