d1488cb009bb25a4a95c6517fedb248c98f5763d
Amazingren <493392637@qq.com>
2024-04-11 00:06:52 +0200

    change sp loss weight

--- devs/base_sp/scripts/pretrain.sh
+++ devs/base_sp/scripts/pretrain.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=6 python main.py \
     --config cfgs/pretrain/base.yaml \
-    --exp_name base_sp
+    --exp_name base_sp0.1
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
