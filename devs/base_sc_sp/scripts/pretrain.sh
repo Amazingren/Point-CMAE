a88feca06df61f34c25727e430522ef749a313c0
Amazingren <493392637@qq.com>
2024-04-10 23:53:34 +0200

    new exps

--- devs/base_sc_sp/scripts/pretrain.sh
+++ devs/base_sc_sp/scripts/pretrain.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=7 python main.py \
     --config cfgs/pretrain/base.yaml \
-    --exp_name base_sc_sp
+    --exp_name base_sc_sp0.1
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
