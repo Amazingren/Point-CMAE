4c57a4346bcce857f0a1bbe9efce33d703d2d278
Amazingren <493392637@qq.com>
2024-04-11 00:12:22 +0200

    sponly, maskratio->0.1

--- devs/base_sponly/scripts/pretrain.sh
+++ devs/base_sponly/scripts/pretrain.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=5 python main.py \
     --config cfgs/pretrain/base.yaml \
-    --exp_name dev_sponly
+    --exp_name dev_sponly_mask0.1
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
