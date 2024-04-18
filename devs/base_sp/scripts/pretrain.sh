eb4ffbdae2d2b9ee8764e3a2f4ee4221a16d51da
Amazingren <493392637@qq.com>
2024-04-18 17:54:26 +0200

    bug fix for sp

--- devs/base_sp/scripts/pretrain.sh
+++ devs/base_sp/scripts/pretrain.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=4 python main.py \
+CUDA_VISIBLE_DEVICES=6 python main.py \
     --config cfgs/pretrain/base.yaml \
-    --exp_name base_sp_m0
+    --exp_name base_sp_dev
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
