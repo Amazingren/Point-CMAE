34918368ffbf5feb18ee056210825a3108f5da46
Amazingren <493392637@qq.com>
2024-04-21 23:44:39 +0200

    exp for seg

--- devs/base_sp/segmentation/seg.sh
+++ devs/base_sp/segmentation/seg.sh
@@ -1,5 +1,5 @@
 python main.py \
     --root /workspace/wwang/BINGO/datasets/point_cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
-    --gpu 5 \
-    --ckpts ../experiments/base/pretrain/dev_baseline2/ckpt-epoch-300.pth \
+    --gpu 4 \
+    --ckpts ../experiments/base/pretrain/base_sp/ckpt-last.pth \
     --seed 0
