6e9fae7854579b39d9730b4d0ffcae6c92faf0c0
Amazingren <493392637@qq.com>
2024-04-23 12:05:30 +0200

    exp setting update

--- devs/base_sponly/scripts/cls.sh
+++ devs/base_sponly/scripts/cls.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=6 python main.py \
+CUDA_VISIBLE_DEVICES=1 python main.py \
     --config cfgs/full/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name scan_hardest_sponly_mask0 \
-    --ckpts experiments/base/pretrain/dev_sponly/ckpt-epoch-300.pth \
+    --exp_name scan_hardest_sponly_mask0_ep250 \
+    --ckpts experiments/base/pretrain/dev_sponly/ckpt-epoch-250.pth \
     --seed 0
 
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/full/finetune_scan_hardest.yaml \
--- devs/base_sponly/scripts/cls_mask0.6.sh
+++ devs/base_sponly/scripts/cls_mask0.6.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=5 python main.py \
+CUDA_VISIBLE_DEVICES=2 python main.py \
     --config cfgs/full/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name scan_hardest_sponly_mask0.6 \
-    --ckpts experiments/base_mask0.6/pretrain/dev_sponly_m0.6/ckpt-epoch-300.pth \
+    --exp_name scan_hardest_sponly_mask0.6_ep250 \
+    --ckpts experiments/base_mask0.6/pretrain/dev_sponly_m0.6/ckpt-epoch-250.pth \
     --seed 0
 
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/full/finetune_scan_hardest.yaml \
