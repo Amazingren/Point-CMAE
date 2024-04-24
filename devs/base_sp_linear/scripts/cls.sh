8c514d722aea7071ca48a6732090ed2edff35ea7
Amazingren <493392637@qq.com>
2024-04-24 09:37:33 +0200

    update

--- devs/base_sp_linear/scripts/cls.sh
+++ devs/base_sp_linear/scripts/cls.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=7 python main.py \
+CUDA_VISIBLE_DEVICES=5 python main.py \
     --config cfgs/full/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name scan_hardest_sp_ep275 \
-    --ckpts experiments/base/pretrain/base_sp/ckpt-epoch-275.pth \
+    --exp_name scan_hardest_sp_ep200 \
+    --ckpts experiments/base/pretrain/base_sp_linear/ckpt-epoch-200.pth \
     --seed 0
 
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/full/finetune_scan_hardest.yaml \
