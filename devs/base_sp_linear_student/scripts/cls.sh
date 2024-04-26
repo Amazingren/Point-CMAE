080c8518bd50af20ac1392a74653811f848cf68a
Amazingren <493392637@qq.com>
2024-04-26 11:30:52 +0200

    exp code update

--- devs/base_sp_linear_student/scripts/cls.sh
+++ devs/base_sp_linear_student/scripts/cls.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=5 python main.py \
+CUDA_VISIBLE_DEVICES=6 python main.py \
     --config cfgs/full/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name scan_hardest_sp_ep225 \
-    --ckpts experiments/base/pretrain/base_sp_linear/ckpt-epoch-225.pth \
+    --exp_name scan_hardest_sp_linear_student_ep300 \
+    --ckpts experiments/base/pretrain/base_sp_linear_student/ckpt-epoch-300.pth \
     --seed 0
 
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/full/finetune_scan_hardest.yaml \
