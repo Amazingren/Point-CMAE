e9b247053ab5f1e9584877fd47009964d0eb16b1
Amazingren <493392637@qq.com>
2024-05-05 07:05:18 +0000

    new exp code

---
+++ devs/v1-5_noMAE_ReBlock_sp_mask0.2_newloss/segmentation/test.sh
@@ -0,0 +1,6 @@
+python main.py \
+    --root /workspace/wwang/BINGO/datasets/point_cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
+    --gpu 5 \
+    --ckpts ./log/part_seg/exp/checkpoints/best_model.pth \
+    --seed 0 \
+    --test
