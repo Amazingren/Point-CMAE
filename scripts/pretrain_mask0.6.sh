d9b501784ad5e2952595c051a81db82534984504
Amazingren <493392637@qq.com>
2024-05-05 15:10:35 +0200

    baseline: smconly, mask=0.6 exp

---
+++ scripts/pretrain_mask0.6.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=6 python main.py \
+    --config cfgs/pretrain/base_mask0.6.yaml \
+    --exp_name Recon_SMC_Only_mask0.6
+
+# Final Command
+# CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
