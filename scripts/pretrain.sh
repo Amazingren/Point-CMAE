5dba52708f53aecbc40ba9fe546dff341d239f2c
Amazingren <493392637@qq.com>
2024-05-05 14:30:16 +0200

    base: sms_only, with reblock exp

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=7 python main.py \
     --config cfgs/pretrain/base.yaml \
-    --exp_name Recon_SM_reproduce
+    --exp_name Recon_SMC_Only
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
