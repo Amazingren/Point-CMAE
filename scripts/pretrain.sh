269644b9ea9fac4d598d2cbed5b67f6346a7a17a
Amazingren <493392637@qq.com>
2024-05-07 08:44:50 +0200

    update

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=7 python main.py \
+CUDA_VISIBLE_DEVICES=5 python main.py \
     --config cfgs/pretrain/base.yaml \
-    --exp_name Recon_SMC_Only
+    --exp_name Recon_MAE0.6_Only
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
