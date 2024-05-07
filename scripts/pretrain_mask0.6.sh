ad401cefa73fd4ba2ce09bd1558aaae2d4fbc17a
Amazingren <493392637@qq.com>
2024-05-07 08:11:15 +0200

    code clean

--- scripts/pretrain_mask0.6.sh
+++ scripts/pretrain_mask0.6.sh
@@ -1,6 +1,7 @@
 CUDA_VISIBLE_DEVICES=6 python main.py \
     --config cfgs/pretrain/base_mask0.6.yaml \
-    --exp_name Recon_SMC_Only_mask0.6
+    --exp_name Recon_SMC_Only_mask0.6 \
+    --resume
 
 # Final Command
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/pretrain/base.yaml --exp_name $2
--- segmentation/seg_mask0.6.sh
+++ segmentation/seg_mask0.6.sh
@@ -2,5 +2,5 @@ python main.py \
     --root /workspace/wwang/BINGO/datasets/point_cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
     --gpu 7 \
     --log_dir ./exp_smconly_mask0 \
-    --ckpts ../experiments/base/pretrain/Recon_SMC_Only/ckpt-epoch-300.pth \
-    --seed 0
+    --ckpts ../experiments/base_mask0.6/pretrain/Recon_SMC_Only_mask0.6/ckpt-epoch-275.pth \
+    --seed 0 
