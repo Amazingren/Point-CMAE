eda7e993d30ccd2b0b11849e90fb92ffb17a4ea6
Amazingren <493392637@qq.com>
2024-05-07 11:17:51 +0000

    update

---
+++ devs/v1-5_noMAE_ReBlock_sp_mask0.2_newloss/cls_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=v1-5_cls
+#SBATCH --nodelist=gcp-eu-2
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=a100-40g:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-gpu=40G
+#SBATCH --output=./joblogs/v1-5_cls_cls_ep300.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/v1-5_cls_ep300.error     # Redirect stderr to a separate error log file
+
+# cuda
+export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
+export PATH=/opt/modules/nvidia-cuda-11.3/bin:$PATH
+
+# gcc
+export PATH=/opt/modules/gcc-10.5.0/bin:$PATH
+
+# envs
+source ~/.bashrc
+micromamba activate point
+
+cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs/v1-5_noMAE_ReBlock_sp_mask0.2_newloss
+
+python main.py \
+    --config cfgs/full/finetune_scan_hardest.yaml \
+    --finetune_model \
+    --exp_name v1-5_noMAE_ReBlock_sp_mask0.2_newloss_ep300 \
+    --ckpts experiments/base/pretrain/v1-5_noMAE_ReBlock_sp_mask0.2_newloss/ckpt-epoch-300.pth \
+    --seed 0
--- devs/v1-6_noMAE_sp_mask0.2_newloss_noReBlock/cls_job.sh
+++ devs/v1-6_noMAE_sp_mask0.2_newloss_noReBlock/cls_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=v1-6
+#SBATCH --job-name=v1-6_cls
 #SBATCH --nodelist=gcp-eu-2
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/v1-6.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/v1-6.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/v1-6_cls.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/v1-6_cls.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -23,6 +23,8 @@ micromamba activate point
 cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs/v1-6_noMAE_sp_mask0_newloss_noReBlock
 
 python main.py \
-    --config cfgs/pretrain/base.yaml \
-    --exp_name v1-6_noMAE_sp_mask0_newloss_noReBlock \
-    --resume
+    --config cfgs/full/finetune_scan_hardest.yaml \
+    --finetune_model \
+    --exp_name v1-6_noMAE_sp_mask0_newloss_noReBlock_ep100 \
+    --ckpts experiments/base/pretrain/v1-6_noMAE_sp_mask0_newloss_noReBlock/ckpt-epoch-100.pth \
+    --seed 0
