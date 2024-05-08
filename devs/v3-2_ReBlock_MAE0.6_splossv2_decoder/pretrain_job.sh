f5021db4337fa7e87453f67ee58bea697b1452e1
Amazingren <493392637@qq.com>
2024-05-08 09:54:10 +0200

    code update

---
+++ devs/v3-2_ReBlock_MAE0.6_splossv2_decoder/pretrain_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=v3-1
+#SBATCH --nodelist=gcp-eu-2
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=a100-40g:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-gpu=40G
+#SBATCH --output=./joblogs/v3-1.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/v3-1.error     # Redirect stderr to a separate error log file
+
+# cuda
+export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
+export PATH=/opt/modules/nvidia-cuda-11.3/bin:$PATH
+
+# gcc
+export PATH=/opt/modules/gcc-10.5.0/bin:$PATH
+
+# envs
+source ~/.bashrc
+micromamba activate point
+
+cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs/v3-0_ReBlock_MAE0.6_splossv2_decoder
+
+python main.py \
+    --config cfgs/pretrain/base.yaml \
+    --exp_name v3-0_ReBlock_MAE0.6_splossv2_decoder
