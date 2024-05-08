6b7b743151c01f569a6756b03eb95ac8d800cdde
Amazingren <493392637@qq.com>
2024-05-08 21:52:25 +0000

    vv

--- devs/v3-1_ReBlock_MAE0.6_splossv2_decoder/cls_job.sh
+++ devs/v3-1_ReBlock_MAE0.6_splossv2_decoder/cls_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=v1-5_cls
+#SBATCH --job-name=v3-1_cls
 #SBATCH --nodelist=gcp-eu-2
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/v1-5_cls_ep300.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/v1-5_cls_ep300.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/v3-1_cls_ep100.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/v3-1_cls_ep100.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -20,11 +20,11 @@ export PATH=/opt/modules/gcc-10.5.0/bin:$PATH
 source ~/.bashrc
 micromamba activate point
 
-cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs/v1-5_noMAE_ReBlock_sp_mask0_newloss
+cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs/v3-1_ReBlock_MAE0.6_splossv2_decoder
 
 python main.py \
     --config cfgs/full/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name v1-5_noMAE_ReBlock_sp_mask0_newloss_ep300 \
-    --ckpts experiments/base/pretrain/v1-5_noMAE_ReBlock_sp_mask0_newloss/ckpt-epoch-300.pth \
+    --exp_name v3-1_ReBlock_MAE0.6_splossv2_decoder_ep100 \
+    --ckpts experiments/base/pretrain/v3-0_ReBlock_MAE0.6_splossv2_decoder/ckpt-epoch-100.pth \
     --seed 0
