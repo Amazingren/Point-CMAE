0977b37b1184675b0a4fcdc8e8faf9bc124541c9
Amazingren <493392637@qq.com>
2024-05-09 17:16:18 +0200

    baselone update

--- scripts/cls.sh
+++ scripts/cls.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=5 python main.py \
+CUDA_VISIBLE_DEVICES=7 python main.py \
     --config cfgs/full/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name scan_hardest_SMConly_mask0_ep300 \
-    --ckpts experiments/base/pretrain/Recon_SMC_Only/ckpt-epoch-300.pth \
+    --exp_name scan_hardest_MAEonly_mask0.6_ep300 \
+    --ckpts experiments/base/pretrain/Recon_MAE0.6_Only/ckpt-epoch-300.pth \
     --seed 0
 
 # CUDA_VISIBLE_DEVICES=$1 python main.py --config cfgs/full/finetune_scan_hardest.yaml \
