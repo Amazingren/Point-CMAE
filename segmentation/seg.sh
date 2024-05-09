87e43bfab5d5cca50abba064e23fb1c8785ad4dd
Amazingren <493392637@qq.com>
2024-05-09 17:13:22 +0200

    update

--- segmentation/seg.sh
+++ segmentation/seg.sh
@@ -1,6 +1,6 @@
 python main.py \
     --root /workspace/wwang/BINGO/datasets/point_cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
-    --gpu 7 \
-    --log_dir ./exp_smconly_mask0 \
-    --ckpts ../experiments/base/pretrain/Recon_SMC_Only/ckpt-epoch-300.pth \
+    --gpu 5 \
+    --log_dir ./exp_MAEonly_mask0.6 \
+    --ckpts ../experiments/base/pretrain/Recon_MAE0.6_Only/ckpt-epoch-300.pth \
     --seed 0
