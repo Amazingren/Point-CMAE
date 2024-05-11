242dc6d87910964a040299bfbdd4ef7faf5fb975
Amazingren <493392637@qq.com>
2024-05-11 07:25:03 +0000

    update

--- devs/v2-1_ReBlock_MAE0.6_moco_splossv1_noProj/segmentation/test.sh
+++ devs/v2-1_ReBlock_MAE0.6_moco_splossv1_noProj/segmentation/test.sh
@@ -1,6 +1,6 @@
 python main.py \
-    --root /workspace/wwang/BINGO/datasets/point_cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
-    --gpu 5 \
-    --ckpts ./log/part_seg/exp/checkpoints/best_model.pth \
-    --seed 0 \
+    --root /data/work-gcp-europe-west4-a/bin_ren/point-cloud/shapenetcore_partanno_segmentation_benchmark_v0_normal/ \
+    --gpu 0 \
+    --ckpts ./log/part_seg/exp_v2-1_ReBlock_MAE0.6_moco_splossv1_noProj_ep250/checkpoints/best_model.pth \
+    --seed 1987 \
     --test
