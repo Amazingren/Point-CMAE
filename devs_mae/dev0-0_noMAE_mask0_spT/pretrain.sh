19e71a38536064222df3326dbb1437a70eaa3f8a
Amazingren <493392637@qq.com>
2024-05-11 22:12:23 +0200

    update

---
+++ devs_mae/dev0-0_noMAE_mask0_spT/pretrain.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=5 python main.py \
+    --config cfgs/pretrain.yaml \
+    --exp_name dev_sp_noMAE_mask0
---
+++ devs_mae/dev0-1_noMAE_maskStu0.6/pretrain.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=6 python main.py \
+    --config cfgs/pretrain.yaml \
+    --exp_name dev0-1_noMAE_maskStu0.6
