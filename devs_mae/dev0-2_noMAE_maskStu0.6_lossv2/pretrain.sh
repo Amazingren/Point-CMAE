f4428c4f6d4411e61af8f54e458091ec1460f308
Amazingren <493392637@qq.com>
2024-05-15 15:03:20 +0000

    code clean

--- devs_mae/dev0-2_noMAE_maskStu0.6_lossv2/pretrain.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=7 python main.py \
-    --config cfgs/pretrain.yaml \
-    --exp_name dev0-2_noMAE_maskStu0.6_lossv2
--- devs_mae/dev0-3_noMAE_maskStu0.6_lossv3/pretrain.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 python main.py \
-    --config cfgs/pretrain.yaml \
-    --exp_name dev0-3_noMAE_maskStu0.6_lossv3
