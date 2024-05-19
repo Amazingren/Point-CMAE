56e4dbf077caa105dcd9990fd20f06e17540d9b1
Amazingren <493392637@qq.com>
2024-05-19 09:21:29 +0200

    update

---
+++ devs_mae/dev_CL_base/cls.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=5 python main.py \
+    --config cfgs/finetune_scan_hardest.yaml \
+    --finetune_model \
+    --exp_name dev_CL_base_ep100 \
+    --ckpts experiments/pretrain/cfgs/debug/ckpt-epoch-100.pth \
+    --seed 0
---
+++ devs_mae/dev_CL_base_cls/pretrain.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=6 python main.py \
+    --config cfgs/pretrain.yaml \
+    --exp_name debug
