fd715ed30bd5c2af115489cd9cc13a80b8686dc8
Amazingren <493392637@qq.com>
2024-05-25 18:42:06 +0200

    update_fbk

--- devs_mae/dev_CL_base/cls.sh
+++ devs_mae/dev_CL_base/cls.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=5 python main.py \
     --config cfgs/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name dev_CL_base_ep100 \
-    --ckpts experiments/pretrain/cfgs/debug/ckpt-epoch-100.pth \
+    --exp_name dev_CL_base_ep300 \
+    --ckpts experiments/pretrain/cfgs/debug/ckpt-epoch-300.pth \
     --seed 0
--- devs_mae/dev_CL_base_cls/cls.sh
+++ devs_mae/dev_CL_base_cls/cls.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=5 python main.py \
+CUDA_VISIBLE_DEVICES=7 python main.py \
     --config cfgs/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name dev_CL_base_cls_ep100 \
-    --ckpts experiments/pretrain/cfgs/debug/ckpt-epoch-100.pth \
+    --exp_name dev_CL_base_cls_ep300 \
+    --ckpts experiments/pretrain/cfgs/debug/ckpt-epoch-300.pth \
     --seed 0
