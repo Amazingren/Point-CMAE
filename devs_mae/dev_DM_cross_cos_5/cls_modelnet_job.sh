4779443060d2866edcad527c9588248f6fa56e2b
Amazingren <493392637@qq.com>
2024-06-04 08:22:39 +0000

    update

--- devs_mae/dev_DM_cross_cos_5/cls_modelnet_job.sh
+++ devs_mae/dev_DM_cross_cos_5/cls_modelnet_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=dev_DM_cross_cos_ep275_modelnet_cls
+#SBATCH --job-name=dev_DM_cross_cos_5_ep275_modelnet_cls
 #SBATCH --nodelist=gcp-eu-2
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/dev_DM_cross_cos_ep275_modelnet_cls.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/dev_DM_cross_cos_ep275_modelnet_cls.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/dev_DM_cross_cos_5_ep275_modelnet_cls.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/dev_DM_cross_cos_5_ep275_modelnet_cls.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -20,11 +20,11 @@ export PATH=/opt/modules/gcc-10.5.0/bin:$PATH
 source ~/.bashrc
 micromamba activate point
 
-cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs_mae/dev_DM_cross_cos
+cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs_mae/dev_DM_cross_cos_5
 
 python main.py \
     --config cfgs/finetune_modelnet.yaml \
     --finetune_model \
-    --exp_name dev_DM_cross_cos_ep275_modelnet_cls \
-    --ckpts experiments/pretrain/cfgs/dev_DM_cross_cos/ckpt-epoch-275.pth \
+    --exp_name dev_DM_cross_cos_5_ep275_modelnet_cls \
+    --ckpts experiments/pretrain/cfgs/dev_DM_cross_cos_mask5/ckpt-epoch-275.pth \
     --seed 0
