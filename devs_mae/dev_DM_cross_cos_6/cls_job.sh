e9c95b9da80cfb21f6150459707d816858a2142e
Amazingren <493392637@qq.com>
2024-06-27 17:57:27 +0000

    ab exp update

--- devs_mae/dev_DM_cross_cos_6/cls_job.sh
+++ devs_mae/dev_DM_cross_cos_6/cls_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=dev_DM_cross_cos_mask0.7_cls_ep250
-#SBATCH --nodelist=gcp-eu-2
+#SBATCH --job-name=ab_linear_hardest_ep275
+#SBATCH --nodelist=gcp-eu-1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/dev_DM_cross_cos_mask0.7_cls_ep250.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/dev_DM_cross_cos_mask0.7_cls_ep250.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/ab_linear_hardest_ep275.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/ab_linear_hardest_ep275.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -18,13 +18,13 @@ export PATH=/opt/modules/gcc-10.5.0/bin:$PATH
 
 # envs
 source ~/.bashrc
-micromamba activate point
+micromamba activate points
 
-cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs_mae/dev_DM_cross_cos
+cd /home/bin_ren/projects/pointcloud/Point-CMAE/devs_mae/dev_DM_cross_cos_6
 
 python main.py \
-    --config cfgs/finetune_scan_hardest.yaml \
+    --config cfgs/linear/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name dev_DM_cross_cos_mask0.7_cls_ep250 \
-    --ckpts experiments/pretrain/cfgs/dev_DM_cross_cos_mask0.7/ckpt-epoch-250.pth \
+    --exp_name ab_linear_hardest_ep275 \
+    --ckpts experiments/pretrain/cfgs/dev_DM_cross_cos/ckpt-epoch-275.pth \
     --seed 0
--- devs_mae/dev_DM_cross_cos_6/cls_objbg_job.sh
+++ devs_mae/dev_DM_cross_cos_6/cls_objbg_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=dev_DM_cross_cos_ep275_objbg_cls
-#SBATCH --nodelist=gcp-eu-2
+#SBATCH --job-name=ab_linear_objbg_ep275 
+#SBATCH --nodelist=gcp-eu-1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/dev_DM_cross_cos_ep275_objbg_cls.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/dev_DM_cross_cos_ep275_objbg_cls.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/ab_linear_objbg_ep275.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/ab_linear_objbg_ep275.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -18,13 +18,13 @@ export PATH=/opt/modules/gcc-10.5.0/bin:$PATH
 
 # envs
 source ~/.bashrc
-micromamba activate point
+micromamba activate points
 
 cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs_mae/dev_DM_cross_cos
 
 python main.py \
-    --config cfgs/finetune_scan_objbg.yaml \
+    --config cfgs/linear/finetune_scan_objbg.yaml \
     --finetune_model \
-    --exp_name dev_DM_cross_cos_ep275_objbg_cls \
+    --exp_name ab_linear_objbg_ep275 \
     --ckpts experiments/pretrain/cfgs/dev_DM_cross_cos/ckpt-epoch-275.pth \
     --seed 0
