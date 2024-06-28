f837155552faf58d71d917f62ece282489622290
Amazingren <493392637@qq.com>
2024-06-28 09:41:11 +0000

    update

--- devs_mae/ab_mae_byol/cls_job.sh
+++ devs_mae/ab_mae_byol/cls_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=ab_mae_byol_275_cls
-#SBATCH --nodelist=gcp-eu-2
+#SBATCH --job-name=ab_byol_cls_ep275
+#SBATCH --nodelist=gcp-eu-1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/ab_mae_byol_275_cls.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/ab_mae_byol_275_cls.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/ab_byol_cls_ep275.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/ab_byol_cls_ep275.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -25,6 +25,6 @@ cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs_mae/ab_mae_byol
 python main.py \
     --config cfgs/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name ab_mae_byol_275_cls \
-    --ckpts experiments/pretrain/cfgs/ab_mae_byol/ckpt-epoch-275.pth \
+    --exp_name ab_byol_cls_ep275 \
+    --ckpts experiments/pretrain/cfgs/ab_byol/ckpt-epoch-275.pth \
     --seed 0
--- devs_mae/ab_mae_moco/cls_job.sh
+++ devs_mae/ab_mae_moco/cls_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=ab_mae_moco_ep275_cls
-#SBATCH --nodelist=gcp-eu-2
+#SBATCH --job-name=ab_moco_ep275_cls
+#SBATCH --nodelist=gcp-eu-1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100-40g:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-gpu=40G
-#SBATCH --output=./joblogs/ab_mae_moco_ep275_cls.log      # Redirect stdout to a log file
-#SBATCH --error=./joblogs/ab_mae_moco_ep275_cls.error     # Redirect stderr to a separate error log file
+#SBATCH --output=./joblogs/ab_moco_ep275_cls.log      # Redirect stdout to a log file
+#SBATCH --error=./joblogs/ab_moco_ep275_cls.error     # Redirect stderr to a separate error log file
 
 # cuda
 export LD_LIBRARY_PATH=/opt/modules/nvidia-cuda-11.3/lib64:$LD_LIBRARY_PATH
@@ -26,6 +26,6 @@ cd /home/bin_ren/projects/pointcloud/pcd_cluster/devs_mae/ab_mae_moco
 python main.py \
     --config cfgs/finetune_scan_hardest.yaml \
     --finetune_model \
-    --exp_name ab_mae_moco_ep275_cls \
-    --ckpts experiments/pretrain/cfgs/ab_mae_moco/ckpt-epoch-275.pth \
+    --exp_name ab_moco_ep275_cls \
+    --ckpts experiments/pretrain/cfgs/ab_moco/ckpt-epoch-275.pth \
     --seed 0
